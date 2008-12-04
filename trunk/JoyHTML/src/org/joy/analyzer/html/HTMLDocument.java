250a96001f0e79a3c8cf3f242cd13cbd1269583c
lamfeeling@126.com <lamfeeling@126.com@4b1823ea-c1c5-11dd-b16b-519e865567fa>
2008-12-04 09:15:13 +0000

    添加了2个类库

--- trunk/JoyHTML/src/org/joy/analyzer/html/HTMLDocument.java
+++ trunk/JoyHTML/src/org/joy/analyzer/html/HTMLDocument.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.joy.analyzer.html;
 
 import org.cyberneko.html.parsers.DOMParser;
@@ -12,16 +11,21 @@ import org.joy.analyzer.Document;
  *
  * @author Lamfeeling
  */
-public class HTMLDocument extends Document{
+public class HTMLDocument extends Document {
 
-    public HTMLDocument createHTMLDocument(String str){
+    private Document doc;
+
+    public HTMLDocument createHTMLDocument(String str) {
         DOMParser parser = new DOMParser();
         Document doc = (Document) parser.getDocument();
+        //TODO: add parser code here
+        
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    protected HTMLDocument(String content) {
+    protected HTMLDocument(String content, Document doc) {
         super(content);
+        this.doc = doc;
     }
 
     @Override
@@ -29,7 +33,7 @@ public class HTMLDocument extends Document{
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    public Anchor[] getAnchors(){
+    public Anchor[] getAnchors() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 }
