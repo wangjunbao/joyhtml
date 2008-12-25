030fdcccc20c5584a2a72e28a4ab354750d4c4ad
lamfeeling@126.com <lamfeeling@126.com@4b1823ea-c1c5-11dd-b16b-519e865567fa>
2008-12-25 04:50:51 +0000

    完善了注释

--- trunk/src/java/org/joy/analyzer/html/Utility.java
+++ trunk/src/java/org/joy/analyzer/html/Utility.java
@@ -5,7 +5,6 @@
 package org.joy.analyzer.html;
 
 import java.io.BufferedReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
