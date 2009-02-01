a2d472894ff7d294440085978dc248f1301de595
lamfeeling@126.com <lamfeeling@126.com@4b1823ea-c1c5-11dd-b16b-519e865567fa>
2009-02-01 08:43:22 +0000

    加入了海量分词功能

--- src/java/org/joy/nlp/HLS.java
+++ src/java/org/joy/nlp/HLS.java
@@ -24,12 +24,9 @@ public class HLS {
       public static void main(String[] args) {
           HLS h=new HLS();
           h.HLSplitInit("");
-          System.out.println(h.HLS_ParagraphProcess("我是杭鼎", 1));
+          System.out.println(h.HLS_ParagraphProcess("胡新华是老大。", 1));
           System.out.println(h.HLS_ParagraphProcess("JOY柳松好。。。。", 0));
           h.HLFreeSplit();
-        
-
-
       }
 
 
--- src/java/org/joy/nlp/HLWordSpliter.java
+++ src/java/org/joy/nlp/HLWordSpliter.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.joy.nlp;
 
 import java.util.Arrays;
@@ -11,91 +10,74 @@ import java.util.Arrays;
  *
  * @author Administrator
  */
-public class HLWordSpliter implements  WordSpliter {
+public class HLWordSpliter implements WordSpliter {
 
-    private static HLS h=null;
+    private static HLS h = null;
     private final static Object waiter = new Object();
 
     public HLWordSpliter() {
-        synchronized(waiter)
-        {
-            if(h==null)
-            {
-                h=new HLS();
+        synchronized (waiter) {
+            if (h == null) {
+                h = new HLS();
                 h.HLSplitInit("");
             }
         }
     }
 
     public void close() {
-        synchronized(waiter)
-        {
-            if(h!=null)
-            {
+        synchronized (waiter) {
+            if (h != null) {
                 h.HLFreeSplit();
-                h=null;
+                h = null;
             }
         }
 
-        
+
     }
 
     public String[] split(String text) {
-       return split(text,false).split(" ");
+        return split(text, false).split(" ");
     }
 
     public String split(String text, boolean isTagged) {
-        synchronized(waiter)
-        {
-            if(h==null)
-            {
+        synchronized (waiter) {
+            if (h == null) {
                 System.out.println("分词系统已经关闭！");
                 return null;
-            }
-            else
-            {
-                return h.HLS_ParagraphProcess(text, isTagged ? 1:0);
+            } else {
+                return h.HLS_ParagraphProcess(text, isTagged ? 1 : 0);
             }
         }
 
-       
+
     }
 
     public Word[] splitToWords(String text) {
-      String[] s=split(text,true).trim().split(" ");
-      Word[] words=new Word[s.length];
-      for(int i=0;i<s.length;i++)
-      {
-          if(s[i]!=null)
-          {
-              String wordText=s[i].split("/")[0];
-              String wordTag=s[i].split("/")[1];
+        String[] s = split(text, true).replaceAll("\\s+/W", "").split(" ");
+        Word[] words = new Word[s.length];
+        for (int i = 0; i < s.length; i++) {
+            System.out.println(s[i]);
+            String wordText = s[i].split("/")[0];
+            String wordTag = s[i].split("/")[1];
 
-              words[i]=new Word(wordText,wordTag);
-          }
-      }
+            words[i] = new Word(wordText, wordTag);
+        }
 
-      return words;
+        return words;
     }
 
+    public static void main(String[] args) {
+        HLWordSpliter h = new HLWordSpliter();
+        System.out.println(h.split("美国中央情报局是   特务间谍机构。", true));
+
+        for (Word w : h.splitToWords("世界十大色狼之柳松大侠！")) {
+            System.out.print(w.getText() + "/" + w.getTag() + " ");
+        }
+        System.out.println("");
 
 
-
-
-     public static void main(String[] args) {
-        HLWordSpliter h= new HLWordSpliter();
-        System.out.println(h.split("美国中央情报局是特务间谍机构。", true));
-
-       for( Word w:h.splitToWords("世界十大色狼之柳松大侠！"))
-       {
-           System.out.print(w.getText()+"/"+w.getTag()+" ");
-       }
-         System.out.println("");
-
-
-        System.out.println(h.split("中国人是好人", false));
+        System.out.println(h.split("我们在领导的帮助下，实现了这个功能。", true));
         h.close();
-        System.out.println(h.split("中国人是好人", false));
+        System.out.println(h.split("中国人  是好人", false));
     }
-
 }
