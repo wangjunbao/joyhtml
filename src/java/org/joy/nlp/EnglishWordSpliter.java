7bd0ffc405eb385d6cba17be79d16bedf6687024
lamfeeling2 <lamfeeling2@4b1823ea-c1c5-11dd-b16b-519e865567fa>
2009-12-15 17:39:57 +0000

    modified the interface of word spliter

---
+++ src/java/org/joy/nlp/EnglishWordSpliter.java
@@ -0,0 +1,35 @@
+package org.joy.nlp;
+
+import java.util.Vector;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class EnglishWordSpliter extends WordSpliter {
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public String[] split(String text) {
+	String[] words = text.replaceAll("[^a-zA-Z0-9\\s]+", "").split("\\s+");
+	return words;
+    }
+
+    @Override
+    public String split(String text, boolean isTagged) {
+	throw new NotImplementedException();
+    }
+
+    @Override
+    public Word[] splitToWords(String text) {
+	String[] res = split(text);
+	Vector<Word> words = new Vector<Word>();
+	for (String t : res) {
+	    words.add(new Word(t + "/x"));
+	}
+	return words.toArray(new Word[0]);
+    }
+
+}
--- src/java/org/joy/nlp/HLWordSpliter.java
+++ src/java/org/joy/nlp/HLWordSpliter.java
@@ -5,15 +5,15 @@
 package org.joy.nlp;
 
 class HLS {
-	static {
-		System.loadLibrary("HLS");
-	}
+    static {
+	System.loadLibrary("HLS");
+    }
 
-	public native boolean HLSplitInit(String path);
+    public native boolean HLSplitInit(String path);
 
-	public native void HLFreeSplit();
+    public native void HLFreeSplit();
 
-	public native String HLS_ParagraphProcess(String content, int bPOSTagged);
+    public native String HLS_ParagraphProcess(String content, int bPOSTagged);
 
 }
 
@@ -22,78 +22,78 @@ class HLS {
  * 
  * @author Administrator
  */
-public class HLWordSpliter implements WordSpliter {
+public class HLWordSpliter extends WordSpliter {
 
-	private static HLS h = null;
-	private final static Object waiter = new Object();
+    private static HLS h = null;
+    private final static Object waiter = new Object();
 
-	public HLWordSpliter() {
-		synchronized (waiter) {
-			if (h == null) {
-				System.out.println("Initialising dicts...");
-				h = new HLS();
-				if (System.getenv("DIC_HOME") != null) {
-					h.HLSplitInit(System.getenv("DIC_HOME"));
-				} else {
-					h.HLSplitInit("./dicts/");
-				}
-			} else {
-				System.err.println("尝试多次初始化分词词典！");
-			}
+    public HLWordSpliter() {
+	synchronized (waiter) {
+	    if (h == null) {
+		System.out.println("Initialising dicts...");
+		h = new HLS();
+		if (System.getenv("DIC_HOME") != null) {
+		    h.HLSplitInit(System.getenv("DIC_HOME"));
+		} else {
+		    h.HLSplitInit("./dicts/");
 		}
+	    } else {
+		System.err.println("尝试多次初始化分词词典！");
+	    }
+	}
+    }
+
+    public void close() {
+	synchronized (waiter) {
+	    if (h != null) {
+		h.HLFreeSplit();
+		h = null;
+	    }
 	}
 
-	public void close() {
-		synchronized (waiter) {
-			if (h != null) {
-				h.HLFreeSplit();
-				h = null;
-			}
-		}
+    }
 
+    public String[] split(String text) {
+	return split(text, false).split(" ");
+    }
+
+    public String split(String text, boolean isTagged) {
+	synchronized (waiter) {
+	    if (h == null) {
+		System.out.println("分词系统已经关闭！");
+		return null;
+	    } else {
+		return h.HLS_ParagraphProcess(text, isTagged ? 1 : 0);
+	    }
 	}
 
-	public String[] split(String text) {
-		return split(text, false).split(" ");
+    }
+
+    public Word[] splitToWords(String text) {
+	String[] s = split(text, true).replaceAll("\\s+/W", "").split(" ");
+	Word[] words = new Word[s.length];
+	for (int i = 0; i < s.length; i++) {
+	    String wordText = s[i].split("/")[0];
+	    String wordTag = s[i].split("/")[1];
+
+	    words[i] = new Word(wordText, wordTag);
 	}
 
-	public String split(String text, boolean isTagged) {
-		synchronized (waiter) {
-			if (h == null) {
-				System.out.println("分词系统已经关闭！");
-				return null;
-			} else {
-				return h.HLS_ParagraphProcess(text, isTagged ? 1 : 0);
-			}
-		}
+	return words;
+    }
 
+    public static void main(String[] args) {
+	HLWordSpliter h = new HLWordSpliter();
+	System.out.println(h.split("圆明园兔首将被拍卖。", true));
+
+	for (Word w : h.splitToWords("他从马上摔下来。")) {
+	    System.out.print(w.getText() + "/" + w.getTag() + " ");
 	}
+	System.out.println("");
+	new HLWordSpliter();
 
-	public Word[] splitToWords(String text) {
-		String[] s = split(text, true).replaceAll("\\s+/W", "").split(" ");
-		Word[] words = new Word[s.length];
-		for (int i = 0; i < s.length; i++) {
-			String wordText = s[i].split("/")[0];
-			String wordTag = s[i].split("/")[1];
-
-			words[i] = new Word(wordText, wordTag);
-		}
-
-		return words;
-	}
-
-	public static void main(String[] args) {
-		HLWordSpliter h = new HLWordSpliter();
-		System.out.println(h.split("圆明园兔首将被拍卖。", true));
-
-		for (Word w : h.splitToWords("他从马上摔下来。")) {
-			System.out.print(w.getText() + "/" + w.getTag() + " ");
-		}
-		System.out.println("");
-		new HLWordSpliter();
-
-		System.out.println(h.split("我们在领导的帮助下，实现了这个功能。", true));
-		h.close();
-		System.out.println(h.split("刘挺拔出宝剑。", false));
-	}
+	System.out.println(h.split("我们在领导的帮助下，实现了这个功能。", true));
+	h.close();
+	System.out.println(h.split("刘挺拔出宝剑。", false));
+    }
 }
